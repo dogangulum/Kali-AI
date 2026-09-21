c3051dee9fb5743d87f4b2d043b24c73a22bcd4d
Dogan Gulum <dogangulum@gmail.com>
2026-09-21 11:03:02 +0300

    Add WhatsApp webhook endpoint

---
+++ api/whatsapp-webhook.js
@@ -0,0 +1,24 @@
+﻿const crypto = require("crypto");
+
+module.exports = async function handler(req, res) {
+  const VERIFY_TOKEN = process.env.META_WHATSAPP_VERIFY_TOKEN;
+
+  if (req.method === "GET") {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      return res.status(200).send(challenge);
+    }
+
+    return res.status(403).send("Forbidden");
+  }
+
+  if (req.method === "POST") {
+    console.log("WhatsApp webhook:", JSON.stringify(req.body));
+    return res.status(200).json({ received: true });
+  }
+
+  return res.status(405).send("Method Not Allowed");
+};
